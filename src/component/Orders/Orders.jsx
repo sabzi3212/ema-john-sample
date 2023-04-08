61d0361c7cc6eb02f7956a11718f4b08520eb017
sabzi3212 <sabbirahmed.lu1@gmail.com>
2023-04-08 15:50:10 +0600

    implemented delete from cart

--- src/component/Orders/Orders.jsx
+++ src/component/Orders/Orders.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-    const cart = useLoaderData();
+
+    const SavedCart = useLoaderData();
+    const [cart, setCart] = useState(SavedCart);
+    const handleRemoveFromCart = (id) =>{
+        console.log(id);
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
     return (
         <div className='shop-container'>
            <div className='review-container'>
                 {
-                    cart.map(product => <ReviewItem key={product.id} product={product}></ReviewItem>)
+                    cart.map(product => <ReviewItem key={product.id} product={product} handleRemoveFromCart={handleRemoveFromCart}></ReviewItem>)
                 }
            </div>
            <div className='cart-container'>
--- src/component/ReviewItem/ReviewItem.jsx
+++ src/component/ReviewItem/ReviewItem.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import './ReviewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping, faCoffee, faTrash } from '@fortawesome/free-solid-svg-icons'
 
-const ReviewItem = ({product}) => {
+const ReviewItem = ({product, handleRemoveFromCart}) => {
     return (
-        <div>
-            <h2>review item</h2>
-            
+        <div className='review-item'>
+            <img src={product.img} />
+            <div className='review-details'>
+            <h2 className='review-title'>{product.name}</h2>
+            <p>Price: <span className='color'>${product.price}</span></p>
+            <p>Shipping Charge: ${product.shipping}</p>
+            </div>
+            <button onClick={()=> handleRemoveFromCart(product.id)} className='btn-delete'><FontAwesomeIcon className='delete-icon' icon={faTrash}/></button>
+
         </div>
     );
 };
